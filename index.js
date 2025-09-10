cec70af389de73a5739693d9245d9f7e193d842b
eugelu11 <34776849+eugelu11@users.noreply.github.com>
2025-09-10 19:30:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = 3000;
 app.use(express.json());
 
 // Rutas
-app.use("/pedido", pedidoRoutes);
+app.use("/pedidos", pedidoRoutes);
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
