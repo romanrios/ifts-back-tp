f47f171c0039d8c7410cc6c9d1deac617ebfe939
eugelu11 <34776849+eugelu11@users.noreply.github.com>
2025-09-10 19:29:40 -0300

    cambio files parte2

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const temarioRoutes = require('./routes/temarioRoutes');
+const pedidoRoutes = require('./routes/temarioRoutes');
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
 // Rutas
-app.use("/temario", temarioRoutes);
+app.use("/pedido", pedidoRoutes);
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
