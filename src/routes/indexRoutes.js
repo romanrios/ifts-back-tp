164fd7e87f3c5d530fb75827eebecb594430abea
Román Ríos <romanrios@live.com>
2025-11-15 12:02:10 -0300

    wip

--- src/routes/indexRoutes.js
+++ src/routes/indexRoutes.js
@@ -1,8 +1,14 @@
 import express from "express";
+import { ensureAuthenticated } from "../middlewares/ensureAuth.js";
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('index');
+router.get("/", ensureAuthenticated, (req, res) => {
+  res.render("index");
+});
+
+router.get("/cuenta", ensureAuthenticated, (req, res) => {
+  res.render("cuenta");
 });
 
 export default router;
